6937ca6a809e1785c02abf7b16090d4c2943f1fb
Roman Dąbal <dabalroman@gmail.com>
2023-04-20 22:24:02 +0200

    Serverless functions test;

---
+++ pages/api/image.js
@@ -0,0 +1,20 @@
+// pages/api/random-image.js
+import sharp from 'sharp';
+
+export default async (req, res) => {
+    // Generate a random image using the sharp library
+    const image = await sharp({
+        create: {
+            width: 400,
+            height: 400,
+            channels: 3,
+            background: { r: 0, g: 0, b: 255 }
+        }
+    })
+        .png()
+        .toBuffer();
+
+    // Set the Content-Type header and send the image as the response
+    res.setHeader('Content-Type', 'image/png');
+    res.send(image);
+};
